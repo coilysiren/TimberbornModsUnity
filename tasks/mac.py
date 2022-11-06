049176a75fdd61b4d3ee3d0a25a3847c7512d260
Lynn <firemagelynn@gmail.com>
2022-11-06 15:40:45 -0500

    copy

--- tasks/mac.py
+++ tasks/mac.py
@@ -1,11 +1,29 @@
+"""
+Mac based build commands
+"""
+
+
 # 3rd party
 import invoke
 
 
 @invoke.task
-def copy_bepinex(ctx):
+def copy_bepinex(ctx, version="5.4.21"):
+    """
+    copy from https://github.com/BepInEx/BepInEx/releases into Timberborn folder
+    """
+    ctx.run("rm -rf ~/Downloads/BepInEx")
+    ctx.run("mkdir -p ~/Downloads/BepInEx")
     ctx.run(
-        r"cp -r ~/Downloads/BepInEx/* ~/Library/Application\ Support/Steam/steamapps/common/Timberborn/",
-        pty=True,
-        echo=True,
+        "wget "
+        "https://github.com/BepInEx/BepInEx/releases/download"
+        f"/v{version}/BepInEx_unix_{version}.0.zip "
+        "-O ~/Downloads/BepInEx/BepInEx.zip"
+    )
+    ctx.run("unzip ~/Downloads/BepInEx/BepInEx.zip")
+    ctx.run("rm ~/Downloads/BepInEx/BepInEx.zip")
+    ctx.run(
+        r"cp -rv "
+        r"~/Downloads/BepInEx/* "
+        r"~/Library/Application\ Support/Steam/steamapps/common/Timberborn/",
     )
